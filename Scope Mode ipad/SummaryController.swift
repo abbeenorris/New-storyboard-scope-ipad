b3635fe5a0eb7e47e0692ff21bdf816e51c0f7c2
Samuel Gray <sam.gray453@gmail.com>
2016-05-12 17:58:29 +0100

    fixing some other things

--- Scope Mode ipad/SummaryController.swift
+++ Scope Mode ipad/SummaryController.swift
@@ -8,7 +8,21 @@ class SummaryController: UIViewController {
     var platformVar = String()
     var descriptionVar = String()
     
-    @IBOutlet weak var Projectt: UITextField!
+    @IBOutlet weak var projectText: UITextField!
+    @IBOutlet weak var videoTypeText: UITextField!
+    @IBOutlet weak var budgetText: UITextField!
+    @IBOutlet weak var platformText: UITextField!
+    @IBOutlet weak var DescriptionText: UITextField!
+    @IBOutlet weak var styleText: UITextField!
+    @IBOutlet weak var styleContinunedText: UITextField!
+    @IBOutlet weak var charactersText: UITextField!
+    @IBOutlet weak var preProductionText: UITextField!
+    @IBOutlet weak var scenesText: UITextField!
+    @IBOutlet weak var partProductionText: UITextField!
+    @IBOutlet weak var deadlineText: UITextField!
+    @IBOutlet weak var postProductionText: UITextField!
+    @IBOutlet weak var runningTimeText: UITextField!
+    @IBOutlet weak var videoSelectedText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
